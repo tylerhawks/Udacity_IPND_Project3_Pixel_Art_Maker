e10ae7fc65754cd801bfed40123363ef173be5c9
tjhawks <hawkscoding@gmail.com>
2019-09-28 12:08:37 -0700

    feat: added game funstionality

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
-// Select color input
 // Select size input
+$("#sizePicker").submit(function(event) {
+  event.preventDefault();
+  height = $("#inputHeight").val();
+  width = $("#inputWidth").val();
+  makeGrid(height, width);
+})
 
-// When size is submitted by the user, call makeGrid()
+function makeGrid(height, width) {
+  // clear old grid
+  $("tr").remove();
 
-function makeGrid() {
+  // loop through value of inputHeight to add rows
+  for (var x = 1; x <= height; x++) {
+    $("#pixelCanvas").append("<tr id=table" + x + "></tr>");
+    // loop through value of inputWidth to add columns
+    for (var y = 1; y <= width; y++) {
+      $("#table" + x).append("<td></td>");
+    }
+  }
 
-// Your code goes here!
+  // Select color input
+  $("td").click(function() {
+    color = $("#colorPicker").val();
+    // clear any existing colors - allows romoving color with second click
+    if ($(this).attr("style")) {
+      $(this).removeAttr("style");
+    // apply new colors
+    } else {
+      $(this).attr("style", "background-color:" + color);
+    }
+  })
 
 }
